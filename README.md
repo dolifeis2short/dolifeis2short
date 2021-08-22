2a468f5c6fc6459166a4815b3374e00fae6224a2
ChoiGW <dolifeis2short@gmail.com>
2021-08-22 17:49:12 +0900

    update 2021.08.22

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ WELCOME!!
   
 Pages Under construction (to. 2021.12.31) - 1 Step  
 Pages Open Limited - (from. 2022. 01. 01  ) - 2 Step  
-OPen (To be notice) - 3 Step  
+Open (To be notice) - 3 Step  
--- assets/js/16.163d7c5d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{382:function(t,e,n){"use strict";n.r(e);var s=n(48),l=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[this._v("(책 및 강의, 트랜드, ETC(여행, 생각))")])])}),[],!1,null,null,null);e.default=l.exports}}]);
